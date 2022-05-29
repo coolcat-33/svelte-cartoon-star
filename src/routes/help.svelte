f87669649485d9f89fb04d318bfa5a7ae8ff2114
coolcat-33 <coolcat-33>
2022-05-29 08:46:06 -0500

    added and updated help page

--- src/routes/help.svelte
+++ src/routes/help.svelte
@@ -1,43 +1,32 @@
-<script>
-    import { TextArea } from "carbon-components-svelte";
-</script>
-
-<!-- 
-<TextArea
-  disabled
-  labelText="What the App is about: "
-/>
-
-
-
-<TextArea
-  disabled
-  labelText="What to do: "
-  placeholder="This app requires you to login with google account. 
-  This app will only work if you are part of the CISD Organization. 
-  Once you sign in with your CISD Google Account, you will be taken to the gallery page. 
-  The gallery page is where you upload your cartoons so people can like them. 
-  On the gallery page you can also like other people's cartoons.
-The Leaderboard page shows you where other people ranked, also including you.
-To Logout, click on the Profile Icon and the click the Icon that says Logout. Refresh the 
-page to log back in."
-/> -->
-
-
 <p>
-    What to do: 
-    <br>
-    <br>
-    
-    <strong>
-        This app requires you to login with google account. 
-        This app will only work if you are part of the <i>CISD</i> Organization. 
-        Once you sign in with your <i>CISD</i> Google Account, you will be taken to the gallery page. 
-        The gallery page is where you upload your cartoons so people can like them. 
-        On the gallery page you can also like other people's cartoons.
-        The Leaderboard page shows you where other people ranked, also including you.
-        To Logout, click on the Profile Icon and the click the Icon that says Logout. Refresh the 
-        page to log back in.
-    </strong>
-    
+  What to do:
+  <br />
+  <br />
+  <strong>
+    This app requires you to login with your google account. This app will only
+    work if you are part of the
+    <i>CISD</i>
+    Organization. Once you sign in with your
+    <i>CISD</i>
+    Google Account, you will be taken to the gallery page. The gallery page is
+    where you upload your cartoons so people can like them. On the gallery page
+    you can also like other people's cartoons. The Leaderboard page shows you
+    where other people ranked, also including you. To Logout, click on the
+    Profile Icon and the click the Icon that says Logout. Refresh the page to
+    log back in.
+  </strong>
+</p>
+<br />
+<br />
+<p>
+  What the App is about:
+  <br />
+  <br />
+  <strong>
+    This app lets you upload cartoons and comics you make for other people to
+    view them and like them. The more likes you get, the more XP you have. The
+    more XP you have, the higher you will be on the Leaderboard. You are
+    competiting to be the best on the Leaderboard, which means you will have to
+    make your best comic.
+  </strong>
 </p>
